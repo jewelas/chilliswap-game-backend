ac1fa55c9bc9550227485948cd091c67860189a2
sayeeddifx <97098669+sayeeddifx@users.noreply.github.com>
2022-04-21 12:07:50 +0400

    code clean up

--- server/service/config.js
+++ server/service/config.js
@@ -1,20 +1,4 @@
 const WSoptions = {
-  timeout: 30000, // ms
-
-  // Useful for credentialed urls, e.g: ws://username:password@localhost:8546
-  // headers: {
-  //   authorization: 'Basic username:password'
-  // },
-
-  clientConfig: {
-    // Useful if requests are large
-    maxReceivedFrameSize: 100000000,   // bytes - default: 1MiB
-    maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
-
-    // Useful to keep a connection alive
-    keepalive: true,
-    keepaliveInterval: 60000 // ms
-  },
 
   // Enable auto reconnection
   reconnect: {
--- server/validations/users.validate.js
+++ server/validations/users.validate.js
@@ -1,21 +1,7 @@
 
 const Joi = require('joi')
-const register = (user) => { //register validations
+ const login = (user) => { //register validations
     const schema = Joi.object({
-      email: Joi.string().email().min(5).max(500).required(),
-      password: Joi.string().min(8).max(1024).required(),
-      username: Joi.string().min(8).max(1024).required(),
-      publicAddress: Joi.string().required(),
-      signature: Joi.string(),
-    })
-    return schema.validate(user)
-  }
-
-  const login = (user) => { //register validations
-    const schema = Joi.object({
-    //   email: Joi.string().email().min(5).max(500).required(),
-    //   password: Joi.string().min(8).max(1024).required(),
-    //   username: Joi.string().min(8).max(1024).required(),
     publicAddress: Joi.string().required(),
     signature: Joi.string().required(),
 
