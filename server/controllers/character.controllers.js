351d17748f9aeabb4ccfcc6dd69e331835d4b05b
unknown <james.lin@difx.io>
2022-05-16 02:15:50 -0400

    fixed character api

--- server/controllers/character.controllers.js
+++ server/controllers/character.controllers.js
@@ -1,14 +1,28 @@
-const character = require('../models/character')
+const Character = require('../models/character')
 
 exports.set = async (req, res) => {
   try {
     const { skintone, hairstyle, eyecolor, clothes, accessories  } = req.body
     
-    await character.updateOne(
-      { "userId": req.user.id },
-      { $push: {"list": { skintone, hairstyle, eyecolor, clothes, accessories }}},
-      {upsert: true}
-    )
+    // await Character.updateOne(
+    //   { "userId": req.user.id },
+    //   { $push: {"list": { skintone, hairstyle, eyecolor, clothes, accessories }}},
+    //   {upsert: true}
+    // )
+    const isExist = await Character.findOne({"userId" : req.user.id});
+    if(isExist) {
+      await Character.updateOne(
+        { "userId": req.user.id },
+        {  "characterData": { skintone, hairstyle, eyecolor, clothes, accessories }},
+      )
+    }
+    else {
+      const newCharacter = new Character ({
+        userId: req.user.id,
+        characterData: { skintone, hairstyle, eyecolor, clothes, accessories }
+      })
+      await newCharacter.save();
+    }
     res.send({status: "success"})
 
   } catch (error) {
@@ -19,7 +33,7 @@ exports.set = async (req, res) => {
 exports.get = async(req, res) => {
   try {
     const userId = req.user.id
-    const data =  await character.find({userId})
+    const data =  await Character.find({userId})
     return res.send(data)
   } catch (error) {
     return res.status(401).send(error.message);
